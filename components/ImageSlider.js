36900ecffadeb2ec689250b5c9614d7239b72303
Sherif Badawy <sherif@mvstack.com>
2024-01-31 09:59:23 -0500

    testing

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -8,7 +8,7 @@ const ImageSlider = ({ imageResults, selectedImages, toggleImageSelection }) => 
     return (
         <div>
             <div className="grid grid-cols-4 gap-4 py-2">
-                {displayedImages.map((image, index) => (
+                {displayedImages?.map((image, index) => (
                     <div key={index} className="relative cursor-pointer">
                         <img src={image.thumbnail} alt={image.title} className="max-h-60 w-full object-cover" onClick={() => toggleImageSelection(image.original)} />
                         {selectedImages.includes(image.original) && (
@@ -20,7 +20,7 @@ const ImageSlider = ({ imageResults, selectedImages, toggleImageSelection }) => 
                     </div>
                 ))}
             </div>
-            {imageResults.length > 12 && !isExpanded && (
+            {imageResults?.length > 12 && !isExpanded && (
                 <button className="flex items-center justify-center w-full mt-4" onClick={() => setIsExpanded(true)}>
                     <PlusIcon className="h-6 w-6 mr-2" /> Show All
                 </button>
