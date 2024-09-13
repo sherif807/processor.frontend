48888d807b1e9beb62c6f1e0f3424b817b06e4cb
Sherif Badawy <sherif@mvstack.com>
2024-09-13 14:02:05 -0400

    testing

--- context/UploadContext.js
+++ context/UploadContext.js
@@ -54,10 +54,10 @@ export const UploadProvider = ({ children }) => {
 
   useEffect(() => {
     const processUploadQueue = async () => {
-      if (isUploading || uploadQueue.length === 0){
-        alert("No files to upload or another upload is in progress.");
-        return;
-      }
+
+      alert(`isUploading: ${isUploading}, Queue Length: ${uploadQueue.length}`);
+
+      if (isUploading || uploadQueue.length === 0) return;
 
       const { file, type } = uploadQueue[0]; // Get file and type from the queue
       setIsUploading(true);
